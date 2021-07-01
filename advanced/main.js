23ea099c36fb17324d90f2d3d8ff9a590415a633
markus kamitz <markus_kamitz@yahoo.de>
2021-07-01 13:21:13 +0200

    advanced final

--- advanced/main.js
+++ advanced/main.js
@@ -1,23 +1,23 @@
 
 let myData = JSON.parse(books);
 var text = "";
-console.log(myData);
 
 for (let val of myData) {
 
-    let flag = val.title;
-    if(flag == true){
+    if(val.read === 'true'){
         text += `
-            <p> 'Title' : ${val.title}  < /p>
-            <p> 'Author': ${val.author}  </p> 
-            <p><img src="${val.author}" > </p>
+            <p style="color:blue"> Title : ${val.title}  </p>
+            <p> Author: ${val.author}  </p> 
+            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
         `
     }   
     else{
         text += `
-            <p> 'Title' : ${val.title}  < /p>
-            <p> 'Author': ${val.author}  </p> 
-            <p><img src="${val.author}" > </p>
+            <p style="color:red"> Title : ${val.title}  </p>
+            <p> Author: ${val.author}  </p> 
+            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
         `
     }    
-    }
+}
+
+document.getElementById("results").innerHTML = text;
