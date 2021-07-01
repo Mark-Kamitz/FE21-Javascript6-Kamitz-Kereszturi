03f8ccc151f450e3f66411ae0da1335c788ad3da
markus kamitz <markus_kamitz@yahoo.de>
2021-07-01 13:30:22 +0200

    challenge start

---
+++ challenge/main.js
@@ -0,0 +1,22 @@
+let myData = JSON.parse(cars);
+var text = "";
+
+for (let val of myData) {
+
+    if(val.read === 'true'){
+        text += `
+            <p style="color:blue"> Title : ${val.title}  </p>
+            <p> Author: ${val.author}  </p> 
+            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
+        `
+    }   
+    else{
+        text += `
+            <p style="color:red"> Title : ${val.title}  </p>
+            <p> Author: ${val.author}  </p> 
+            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
+        `
+    }    
+}
+
+document.getElementById("results").innerHTML = text;
