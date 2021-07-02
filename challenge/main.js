afd97888ebc600b5f806fed1fa5682d51448ba38
markus kamitz <markus_kamitz@yahoo.de>
2021-07-02 08:20:09 +0200

    challenge button and function added - only working 1 time :(

--- challenge/main.js
+++ challenge/main.js
@@ -1,22 +1,53 @@
-let myData = JSON.parse(cars);
-var text = "";
 
-for (let val of myData) {
 
-    if(val.read === 'true'){
-        text += `
-            <p style="color:blue"> Title : ${val.title}  </p>
-            <p> Author: ${val.author}  </p> 
-            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
+let mydata = JSON.parse(cars);
+console.log(mydata);
+var text = "" ;
+
+text +=  ` <div class="row " id="showMore"> `; 
+ 
+    for (let val of mydata) {
+    text += `
+        <div class="col-12 col-sm-12 col-md-6 col-lg-3 mb-4">
+            <div class="card" container-fluid">
+                <img src="${val.image}" style="object-fit:cover; width:100%; height:150px;" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${val.name}</h5>
+                    <a href="#" class="btn btn-warning showMoreInfo">More Info</a>
+                </div>
+            </div> 
+        </div>
         `
-    }   
-    else{
-        text += `
-            <p style="color:red"> Title : ${val.title}  </p>
-            <p> Author: ${val.author}  </p> 
-            <p style="width:150px;"><img src="${val.cover}" style="with:100%; height:auto"> </p>
-        `
-    }    
-}
+    }
+  
+text += ` </div>` ; 
 
 document.getElementById("results").innerHTML = text;
+
+var btns = document.getElementsByClassName("showMoreInfo");
+
+for(let i = 0; i < btns.length; i++) {
+     btns[i].addEventListener("click", function(){printMore(i)});
+}
+
+function printMore(index) {
+    document.getElementById("showMore").innerHTML += `
+    <div class="col-12 col-sm-12 col-md-12 col-lg-12 mb-4 p-5">
+        <div class="card" container-fluid">
+            <img src="${mydata[index].image}" style="object-fit:cover; width:100%; height:150px;" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${mydata[index].name}</h5>
+                <p class="card-text">Price: ${mydata[index].price}</p>
+                <p class="card-text">Production Year: ${mydata[index].productionYear}</p>
+            </div>
+        </div> 
+    </div>
+    `
+}
+
+
+
+
+
+
+
